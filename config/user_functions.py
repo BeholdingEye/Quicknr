fa93efa4cbce04104c252dabcac7f2a430ec5e17
Beholding Eye <BeholdingEye@users.noreply.github.com>
2016-02-16 18:17:27 +0100

    Create user_functions.py

---
+++ config/user_functions.py
@@ -0,0 +1,75 @@
+########################################################################
+#                                                                      #
+#                       QUICKNR USER FUNCTIONS                         #
+#                                                                      #
+#  Define the functions that will be called from HTML snippets with    #
+#  '@python: "function-name"' directives to work on completed HTML     #
+#  output of a converted source just before it is written to file.     #
+#                                                                      #
+#  The function must accept exactly three positional parameters:       #
+#                                                                      #
+#  1) Text of the full HTML output just before writing to file         #
+#                                                                      #
+#  2) Starting index position of the function directive in the text    #
+#                                                                      #
+#  3) Configuration dictionary holding website settings. Keys of the   #
+#     dictionary have the same names as options in the "config.txt"    #
+#     file. Some additional keys are also present: "siteDir" for       #
+#     website folder path, "siteFolder" for its folder name, and the   #
+#     self-explanatory "sourceFilePath" and "htmlFilePath" of the      #
+#     file being worked on. The dictionary is read-only.               #
+#                                                                      #
+#  The function directive is removed from the text before the call is  #
+#  made. After it has done its work, the function must return the      #
+#  processed text.                                                     #
+#                                                                      #
+#  This file as a whole will be read and executed by Quicknr before    #
+#  the relevant function is called. It is therefore safest to not      #
+#  include any code outside function (or class) definitions.           #
+#                                                                      #
+########################################################################
+
+
+def latest_news_link(text, i, CD):
+    """
+    Imports news listing page link into news pages, taking account of CD preference
+    for the listing page name (by default "Latest News")
+    
+    """
+    hCode = """
+<div class="news_listing_link">
+<a href="../news.html">{}</a>
+</div>
+    """
+    if os.path.split(os.path.dirname(CD["sourceFilePath"]))[1] == "news":
+        hCode = hCode.format(CD["NEWS_LIST_TITLE"]).strip()+"\n"
+        return text[:i] + hCode + text[i:]    
+    else: return text
+
+
+def page_style_link(text, i, CD):
+    """
+    Adds stylesheet link(s) to matching .css file(s) for some pages - adapt to 
+    your own needs
+    
+    """
+    import os
+    # Values are lists, so that more than one stylesheet can import into a file
+    pairsD = {
+                    "index.txt"     :   ["index.css"],
+                    "about.txt"     :   ["about.css"],
+                    "news.txt"      :   ["newslist.css"],
+                    "newspost"      :   ["newspost.css"]  } # Will catch any name
+    pageMatch = os.path.basename(CD["sourceFilePath"])
+    link = '<link rel="stylesheet" href="res/css/{}" type="text/css" />\n'
+    # Alter the link for news posts
+    if os.path.split(os.path.dirname(CD["sourceFilePath"]))[1] == "news":
+        link = '<link rel="stylesheet" href="../res/css/{}" type="text/css" />\n'
+        pageMatch = "newspost"
+    try:
+        nlink = ""
+        for x in pairsD[pageMatch]:
+            nlink += link.format(x)
+    except KeyError: return text
+    return text[:i] + nlink + text[i:]
+
